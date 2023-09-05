f2078d0276423c1f9eff0c7202d3141a0091d823
Gohar Ali <goharali16@gmail.com>
2023-09-05 16:45:00 +0500

    Initial setup: Advanced Flutter weather app
    - Create comprehensive weather application with modern architecture
    - Set up Riverpod state management and location services
    - Add multiple weather API integrations (OpenWeatherMap, WeatherAPI)
    - Implement detailed forecasts with hourly and daily predictions
    - Configure interactive radar maps with real-time weather data
    - Add weather alerts and severe weather notifications
    - Include AI-powered weather insights and recommendations
    - Set up background weather updates and location tracking
    
    Core Features:
    - Real-time weather data with multiple sources
    - Interactive radar and satellite maps
    - Detailed 7-day and hourly forecasts
    - Weather alerts and severe weather warnings
    - Location-based weather tracking
    - Weather widgets and home screen integration
    - Historical weather data and trends
    - AI weather predictions and insights

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/config/app_config.dart';
+import 'core/theme/app_theme.dart';
+import 'core/routing/app_router.dart';
+import 'core/services/weather_service.dart';
+import 'core/services/location_service.dart';
+import 'core/services/notification_service.dart';
+import 'features/weather/presentation/providers/weather_provider.dart';
+import 'features/location/presentation/providers/location_provider.dart';
+import 'features/alerts/presentation/providers/alerts_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize services
+  await WeatherService.initialize();
+  await LocationService.initialize();
+  await NotificationService.initialize();
+  
+  runApp(
+    const ProviderScope(
+      child: WeatherProApp(),
+    ),
+  );
+}
+
+class WeatherProApp extends ConsumerWidget {
+  const WeatherProApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeModeProvider);
+    
+    return MaterialApp.router(
+      title: AppConfig.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
+      routerConfig: router,
+    );
+  }
+}
